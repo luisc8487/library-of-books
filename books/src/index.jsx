a3625e66215f7aa67f87a8e8bdfb16b38b65b9c0
L C <LF9904@gmail.com>
2025-05-11 20:07:56 -0400

    inital setup

---
+++ books/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReacDOM from "react-dom/client";
+import App from "./App";
+
+const el = document.getElementById("root");
+const root = ReacDOM.createRoot(el);
+
+root.render(<App />);
