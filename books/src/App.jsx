59a786b1ba557836b692dfccfad25a61d1067e18
L C <LF9904@gmail.com>
2025-05-14 15:01:57 -0400

    generated random IDs and displayed the list of books created

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,15 +1,21 @@
 import {useState} from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("Need to add book with:", title);
+    const updatedBooks = [
+      ...books,
+      {id: Math.random(Math.random() * 9999), title},
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
