3cdba1275ca9aedc8230fb3b20b8407d4ba5d786
L C <LF9904@gmail.com>
2025-05-19 12:47:15 -0400

    added delete function for server

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -16,7 +16,9 @@ function App() {
     fetchBooks();
   }, []);
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
